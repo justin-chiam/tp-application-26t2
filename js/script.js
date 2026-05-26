f68be74204ead227377e54d7e637759fb64befe1
Justin Chiam <235694260+justin-chiam@users.noreply.github.com>
2026-05-26 10:32:43 +1000

    Save theme to local storage to keep theme across webpages

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
-function toggleTheme() {
-    document.body.classList.toggle("night-mode");
+function applySavedTheme() {
+    const isNightMode = localStorage.getItem("theme") === "night";
+    
+    // Toggle night mode if saved and check button
+    document.body.classList.toggle("night-mode", isNightMode);
+    document.getElementById("theme-toggle").checked = isNightMode;
 }
+
+function toggleTheme() {
+    const isNightMode = document.body.classList.toggle("night-mode");
+
+    // Set theme in local storage
+    if (isNightMode) {
+        localStorage.setItem("theme", "night");
+    } else {
+        localStorage.setItem("theme", "day");
+    }
+}
+
+document.addEventListener("DOMContentLoaded", applySavedTheme);
